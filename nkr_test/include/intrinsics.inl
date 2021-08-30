1662ea3a97cf693caa38aace275f1ea1b27cc331
r-neal-kelly <r.neal.kelly@gmail.com>
2021-08-30 12:13:22 -0400

    improved the random functionality for integers and floats. the float one now proportionally distributes the probability of getting a negative and positive and the integer one now accepts signed numbers. I do think we'll move this functionality over to the library because we had to really work the real distribution to get what really ought to be in the standard.

---
+++ nkr_test/include/intrinsics.inl
@@ -0,0 +1,62 @@
+/*
+    Copyright 2021 r-neal-kelly
+*/
+
+#pragma once
+
+#include "intrinsics.h"
+
+namespace nkr {
+
+    template <boolean_tr boolean_p>
+    inline boolean_p Random(float_t probability_for_true = 0.5)
+    {
+        std::lock_guard<std::mutex> locker(random_lock);
+        return std::bernoulli_distribution(probability_for_true)(random_generator);
+    }
+
+    template <integer_tr integer_p>
+    inline integer_p Random(integer_p from_inclusive = std::numeric_limits<integer_p>::lowest(),
+                            integer_p to_inclusive = std::numeric_limits<integer_p>::max())
+    {
+        std::lock_guard<std::mutex> locker(random_lock);
+        return std::uniform_int_distribution<integer_p>(from_inclusive, to_inclusive)(random_generator);
+    }
+
+    template <float_tr float_p>
+    inline float_p Random(float_p from_inclusive = std::numeric_limits<float_p>::lowest(),
+                          float_p to_inclusive = std::numeric_limits<float_p>::max())
+    {
+        bool_t do_negative = false;
+        if (from_inclusive < 0.0) {
+            float_p abs_from_inclusive = abs(from_inclusive);
+            float_p abs_to_inclusive = abs(to_inclusive);
+            if (to_inclusive > 0.0) {
+                if (abs_from_inclusive > abs_to_inclusive) {
+                    do_negative = !Random<bool_t>(abs_to_inclusive / abs_from_inclusive / 2);
+                } else {
+                    do_negative = Random<bool_t>(abs_from_inclusive / abs_to_inclusive / 2);
+                }
+                if (do_negative) {
+                    from_inclusive = 0.0;
+                    to_inclusive = abs_from_inclusive;
+                } else {
+                    from_inclusive = 0.0;
+                    to_inclusive = abs_to_inclusive;
+                }
+            } else {
+                do_negative = true;
+                from_inclusive = abs_to_inclusive;
+                to_inclusive = abs_from_inclusive;
+            }
+        }
+
+        std::lock_guard<std::mutex> locker(random_lock);
+        if (do_negative) {
+            return -std::uniform_real_distribution<float_p>(from_inclusive, to_inclusive)(random_generator);
+        } else {
+            return std::uniform_real_distribution<float_p>(from_inclusive, to_inclusive)(random_generator);
+        }
+    }
+
+}
--- nkr_test/src/intrinsics.cpp
+++ nkr_test/src/intrinsics.cpp
@@ -11,3 +11,137 @@ namespace nkr {
     std::mutex          random_lock;
 
 }
+
+namespace nkr {
+
+    TEST_SUITE("Random")
+    {
+        constexpr count_t RANDOM_ITERATION_COUNT = 256;
+
+        TEST_SUITE("Random<integer_p>")
+        {
+            TEST_CASE("should by default return both positive and negative numbers for signed integers")
+            {
+                bool_t got_positive = false;
+                bool_t got_negative = false;
+
+                for (index_t idx = 0, end = RANDOM_ITERATION_COUNT; idx < end; idx += 1) {
+                    s32_t random = Random<s32_t>();
+                    if (random > 0) {
+                        got_positive = true;
+                    } else if (random < 0) {
+                        got_negative = true;
+                    }
+                }
+
+                CHECK(got_positive && got_negative);
+            }
+
+            TEST_CASE("should be capable of only returning positive numbers for signed integers")
+            {
+                bool_t got_positive = false;
+                bool_t got_negative = false;
+
+                for (index_t idx = 0, end = RANDOM_ITERATION_COUNT; idx < end; idx += 1) {
+                    s32_t random = Random<s32_t>(0, std::numeric_limits<s32_t>::max());
+                    if (random > 0) {
+                        got_positive = true;
+                    } else if (random < 0) {
+                        got_negative = true;
+                    }
+                }
+
+                CHECK(got_positive && !got_negative);
+            }
+
+            TEST_CASE("should be capable of only returning negative numbers for signed integers")
+            {
+                bool_t got_positive = false;
+                bool_t got_negative = false;
+
+                for (index_t idx = 0, end = RANDOM_ITERATION_COUNT; idx < end; idx += 1) {
+                    s32_t random = Random<s32_t>(std::numeric_limits<s32_t>::lowest(), 0);
+                    if (random > 0) {
+                        got_positive = true;
+                    } else if (random < 0) {
+                        got_negative = true;
+                    }
+                }
+
+                CHECK(!got_positive && got_negative);
+            }
+        }
+
+        TEST_SUITE("Random<float_p>")
+        {
+            TEST_CASE("should by default return both positive and negative numbers")
+            {
+                bool_t got_positive = false;
+                bool_t got_negative = false;
+
+                for (index_t idx = 0, end = RANDOM_ITERATION_COUNT; idx < end; idx += 1) {
+                    float_t random = Random<float_t>();
+                    if (random > 0.0) {
+                        got_positive = true;
+                    } else if (random < 0.0) {
+                        got_negative = true;
+                    }
+                }
+
+                CHECK(got_positive && got_negative);
+            }
+
+            TEST_CASE("should be capable of only returning positive numbers")
+            {
+                bool_t got_positive = false;
+                bool_t got_negative = false;
+
+                for (index_t idx = 0, end = RANDOM_ITERATION_COUNT; idx < end; idx += 1) {
+                    float_t random = Random<float_t>(0.0, std::numeric_limits<float_t>::max());
+                    if (random > 0.0) {
+                        got_positive = true;
+                    } else if (random < 0.0) {
+                        got_negative = true;
+                    }
+                }
+
+                CHECK(got_positive && !got_negative);
+            }
+
+            TEST_CASE("should be capable of only returning negative numbers")
+            {
+                bool_t got_positive = false;
+                bool_t got_negative = false;
+
+                for (index_t idx = 0, end = RANDOM_ITERATION_COUNT; idx < end; idx += 1) {
+                    float_t random = Random<float_t>(std::numeric_limits<float_t>::lowest(), 0.0);
+                    if (random > 0.0) {
+                        got_positive = true;
+                    } else if (random < 0.0) {
+                        got_negative = true;
+                    }
+                }
+
+                CHECK(!got_positive && got_negative);
+            }
+
+            TEST_CASE("should proportionally distribute the probability of getting a negative or positive number")
+            {
+                count_t positive_count = 0;
+                count_t negative_count = 0;
+
+                for (index_t idx = 0, end = RANDOM_ITERATION_COUNT; idx < end; idx += 1) {
+                    float_t random = Random<float_t>(-1.0, 2.0);
+                    if (random > 0.0) {
+                        positive_count += 1;
+                    } else if (random < 0.0) {
+                        negative_count += 1;
+                    }
+                }
+
+                CHECK(positive_count > negative_count);
+            }
+        }
+    }
+
+}
