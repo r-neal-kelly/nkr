bd58e297722b9b16e6fa67c542f98c65c4617b60
r-neal-kelly <r.neal.kelly@gmail.com>
2021-09-08 23:56:02 -0400

    added the skeleton for pointer_t tests

--- nkr/include/nkr/pointer_t.h
+++ nkr/include/nkr/pointer_t.h
@@ -9,18 +9,31 @@
 
 namespace nkr {
 
+    /// @nosubgrouping
+    /// @copydoc 
     template <type_tr unit_p>
     class pointer_t
     {
     public:
+        /// @name aliases
+        /// @copydoc 
+        /// @{
         using unit_t    = unit_p;
         using units_t   = unit_p*;
+        /// @}
 
     protected:
+        /// @name object data
+        /// @copydoc 
+        /// @{
         units_t units;
         count_t unit_count;
+        /// @}
 
     public:
+        /// @name objects
+        /// @copydoc 
+        /// @{
         pointer_t();
         pointer_t(unit_t* unit);
         pointer_t(units_t units, count_t unit_count);
@@ -30,15 +43,27 @@ namespace nkr {
         pointer_t& operator =(const pointer_t& other);
         pointer_t& operator =(pointer_t&& other) noexcept;
         ~pointer_t();
+        /// @}
 
     public:
+        /// @name methods
+        /// @copydoc 
+        /// @{
         units_t Units() volatile const;
         count_t Unit_Count() volatile const;
+        /// @}
 
     public:
+        /// @name casts
+        /// @copydoc 
+        /// @{
         operator    units_t() volatile const;
+        /// @}
 
     public:
+        /// @name operators
+        /// @copydoc 
+        /// @{
         units_t     operator ()() volatile const;
         pointer_t&  operator ()(unit_t* unit);
         pointer_t&  operator ()(units_t units, count_t unit_count);
@@ -57,6 +82,7 @@ namespace nkr {
         units_t     operator ->() const;
         unit_t&     operator *() const;
         unit_t&     operator [](index_t index) const;
+        /// @}
     };
 
 }
--- nkr_test/src/pointer_t.cpp
+++ nkr_test/src/pointer_t.cpp
@@ -10,6 +10,175 @@
 
 namespace nkr {
 
+    TEST_SUITE("pointer_t<unit_p>")
+    {
+        TEST_SUITE("aliases")
+        {
+            TEST_SUITE("unit_t")
+            {
 
+            }
+
+            TEST_SUITE("units_t")
+            {
+
+            }
+        }
+
+        TEST_SUITE("object data")
+        {
+            TEST_SUITE("units")
+            {
+
+            }
+
+            TEST_SUITE("unit_count")
+            {
+
+            }
+        }
+
+        TEST_SUITE("objects")
+        {
+            TEST_SUITE("default_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("unit_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("units_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("copy_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("move_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("unit_assignment_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("copy_assignment_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("move_assignment_ctor()")
+            {
+
+            }
+
+            TEST_SUITE("dtor()")
+            {
+
+            }
+        }
+
+        TEST_SUITE("methods")
+        {
+            TEST_SUITE("Units()")
+            {
+
+            }
+
+            TEST_SUITE("Unit_Count()")
+            {
+
+            }
+        }
+
+        TEST_SUITE("casts")
+        {
+            TEST_SUITE("units_t()")
+            {
+
+            }
+        }
+
+        TEST_SUITE("operators")
+        {
+            TEST_SUITE("()()")
+            {
+
+            }
+
+            TEST_SUITE("()(unit_t*)")
+            {
+
+            }
+
+            TEST_SUITE("()(units_t, unit_count_t)")
+            {
+
+            }
+
+            TEST_SUITE("+()")
+            {
+
+            }
+
+            TEST_SUITE("-()")
+            {
+
+            }
+
+            TEST_SUITE("+=()")
+            {
+
+            }
+
+            TEST_SUITE("-=()")
+            {
+
+            }
+
+            TEST_SUITE("++()")
+            {
+
+            }
+
+            TEST_SUITE("++(int)")
+            {
+
+            }
+
+            TEST_SUITE("--()")
+            {
+
+            }
+
+            TEST_SUITE("--(int)")
+            {
+
+            }
+
+            TEST_SUITE("->()")
+            {
+
+            }
+
+            TEST_SUITE("*()")
+            {
+
+            }
+
+            TEST_SUITE("[]()")
+            {
+
+            }
+        }
+    }
 
 }
