85b62fa242f1ff879965d299077d27ac40c7adac
r-neal-kelly <r.neal.kelly@gmail.com>
2021-10-16 10:48:05 -0400

    added files for string::stack_t. trying to update access tokens which is always a pain because I never remember how to do it.

---
+++ nkr/include/nkr/string/stack_t.h
@@ -0,0 +1,8 @@
+/*
+    Copyright 2021 r-neal-kelly
+*/
+
+#pragma once
+
+#include "nkr/string/stack_t_dec.h"
+#include "nkr/string/stack_t_def.h"
---
+++ nkr/include/nkr/string/stack_t_dec.h
@@ -0,0 +1,20 @@
+/*
+    Copyright 2021 r-neal-kelly
+*/
+
+#pragma once
+
+#include "nkr/intrinsics.h"
+
+#include "nkr/charcoder_i.h"
+#include "nkr/charcoder/utf_8_t.h"
+
+namespace nkr { namespace string {
+
+    template <charcoder_i charcoder_p = charcoder::utf_8_t>
+    class stack_t
+    {
+    public:
+    };
+
+}}
