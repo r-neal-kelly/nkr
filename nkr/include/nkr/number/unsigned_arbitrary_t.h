3bf7f10f1320b39faf4c9c2a4bb69fb9bbf41100
r-neal-kelly <r.neal.kelly@gmail.com>
2021-12-01 10:18:59 -0500

    started cleaning up and rearranging the new arbitrary arithmetic functionality and added a couple of more useful functions for them which have allowed us to optimize the performance of the Karatsuba_Multiply function by removing the need to allocate a buffer. There's more to be done performance wise of course, but this is a great start.

---
+++ nkr/include/nkr/number/unsigned_arbitrary_t.h
@@ -0,0 +1,8 @@
+/*
+    Copyright 2021 r-neal-kelly
+*/
+
+#pragma once
+
+#include "nkr/number/unsigned_arbitrary_t_dec.h"
+#include "nkr/number/unsigned_arbitrary_t_def.h"
---
+++ nkr/include/nkr/number/unsigned_arbitrary_t_dec.h
@@ -0,0 +1,53 @@
+/*
+    Copyright 2021 r-neal-kelly
+*/
+
+#pragma once
+
+#include "nkr/intrinsics.h"
+#include "nkr/maybe_t.h"
+#include "nkr/traits.h"
+
+#include "nkr/allocator_err.h"
+
+#include "nkr/array_i.h"
+#include "nkr/array/static_t.h"
+
+namespace nkr { namespace number {
+
+    // these are temporarily loose
+
+    template <integer_unsigned_tr unit_p>
+    maybe_t<allocator_err>  Add(tr2<any_tg, array::static_t, of_any_tg, unit_p> auto number_a,
+                                tr2<any_tg, array::static_t, of_any_tg, unit_p> auto number_b,
+                                tr2<any_tg, aggregate_array_ttg, of_any_tg, unit_p> auto& result);
+    template <integer_unsigned_tr unit_p>
+    void_t                  Add_In_Place(tr2<any_non_const_tg, array_ttg, of_any_non_const_tg, unit_p> auto& number_a,
+                                         const tr2<any_tg, array_ttg, of_any_tg, unit_p> auto& number_b);
+
+    template <integer_unsigned_tr unit_p>
+    maybe_t<allocator_err>  Subtract(tr2<any_tg, array::static_t, of_any_tg, unit_p> auto number_a,
+                                     tr2<any_tg, array::static_t, of_any_tg, unit_p> auto number_b,
+                                     tr2<any_tg, aggregate_array_ttg, of_any_tg, unit_p> auto& result);
+
+    template <integer_unsigned_tr unit_p>
+    void_t                  Subtract_In_Place(tr2<any_non_const_tg, array_ttg, of_any_non_const_tg, unit_p> auto& number_a,
+                                              const tr2<any_tg, array_ttg, of_any_tg, unit_p> auto& number_b);
+
+    template <integer_unsigned_tr unit_p>
+    void_t                  Karatsuba_Multiply(tr2<any_tg, array::static_t, of_any_tg, unit_p> auto number_a,
+                                               tr2<any_tg, array::static_t, of_any_tg, unit_p> auto number_b,
+                                               tr2<any_tg, aggregate_array_ttg, of_any_tg, unit_p> auto& result);
+
+    template <integer_unsigned_tr unit_p>
+    class unsigned_arbitrary_t
+    {
+    public:
+        using unit_t    = unit_p;
+
+    public:
+        // I want the ctor to take an array of numbers in reverse I think, so it's easier to read static data?
+        // but interally it will always be in little endian, least significant byte first
+    };
+
+}}
