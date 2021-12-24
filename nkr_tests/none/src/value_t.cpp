485046bee7984c50aa6e00486e7a3536a5e46970
r-neal-kelly <r.neal.kelly@gmail.com>
2021-12-24 11:13:16 -0500

    just wanted to show a bit more code using the new types. It's pretty clear that the code is generally harder to read when directly using all of the namespaces. However, it's easy to alias when appropiate. I think the trade-off is a good one though, because it combines clarity with specificity pretty well.

--- nkr_tests/none/src/value_t.cpp
+++ nkr_tests/none/src/value_t.cpp
@@ -2,19 +2,49 @@
     Copyright 2021 r-neal-kelly
 */
 
+#include "nkr/pointer/cpp_t.h"
+
 #include "nkr/positive/integer_t.h"
 
+#include "nkr/none/pointer_t.h"
 #include "nkr/none/value_t.h"
 
 #include "doctest.h"
 
-namespace nkr { namespace none {
+namespace nkr {
 
     TEST_CASE("temp")
     {
-        value_t<positive::integer_t> none = value_t<positive::integer_t>();
+        none::value_t<positive::integer_t> none = none::value_t<positive::integer_t>();
 
         CHECK(static_cast<positive::integer_t>(none) == 0);
+
+        positive::integer_t integer = none::value_t<positive::integer_t>();
+
+        CHECK(integer == 0);
     }
 
-}}
+    TEST_CASE("temp")
+    {
+        none::value_t<pointer::cpp_t<positive::integer_t>> none = none::value_t<pointer::cpp_t<positive::integer_t>>();
+
+        CHECK(static_cast<pointer::cpp_t<positive::integer_t>>(none) == pointer::cpp_t<positive::integer_t>());
+        CHECK(static_cast<pointer::cpp_t<positive::integer_t>>(none) == none::pointer_t());
+
+        pointer::cpp_t<positive::integer_t> pointer = none::value_t<pointer::cpp_t<positive::integer_t>>();
+
+        CHECK(pointer == pointer::cpp_t<positive::integer_t>());
+        CHECK(pointer == none::pointer_t());
+    }
+
+    TEST_CASE("temp")
+    {
+        using integer_t = positive::integer_t;
+        using none_t    = none::value_t<integer_t>;
+
+        integer_t integer = none_t();
+
+        CHECK(integer == 0);
+    }
+
+}
