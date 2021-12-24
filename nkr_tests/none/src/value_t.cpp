b67bcaf4ad84518cb9711750c6af0ad77cd6de51
r-neal-kelly <r.neal.kelly@gmail.com>
2021-12-24 12:02:43 -0500

    okay, so we have to rework the none::value_t. Apparently, there is no way to prevent the implicit cast to the non_none_default_t in the new test case. I think probably the best solution at this point is to have a new interface::none::value_i where you can explicitly define what the none::value_t will implicitly cast to. I think, as long as the casted type is either the template parameter type itself or a value that that type has a constructor for, it should work. We can of course default the interface to the default constructed object. Because of this, we can probably get away with simply requiring the trait::constructor::default_tr for the default none_i implementation.

--- nkr_tests/none/src/value_t.cpp
+++ nkr_tests/none/src/value_t.cpp
@@ -47,4 +47,64 @@ namespace nkr {
         CHECK(integer == 0);
     }
 
+    class non_none_default_t;
+
+    namespace interface
+    {
+        template <>
+        class type_i<non_none_default_t>
+        {
+        public:
+            using of_t  = none::type_t;
+
+        public:
+            template <typename other_p>
+            static constexpr boolean::cpp_t
+                Is_Any()
+                noexcept
+            {
+                return cpp::is_any_tr<other_p, non_none_default_t>;
+            }
+        };
+    };
+
+    class non_none_default_t
+    {
+    public:
+        static constexpr const positive::integer_t  NONE = 1;
+
+    private:
+        positive::integer_t value;
+
+    public:
+        non_none_default_t() :
+            value(0)
+        {
+        }
+
+        non_none_default_t(positive::integer_t value) :
+            value(value)
+        {
+        }
+
+        non_none_default_t(const non_none_default_t& other)                 = delete;
+        non_none_default_t(non_none_default_t&& other) noexcept             = delete;
+
+        non_none_default_t& operator =(const non_none_default_t& other)     = delete;
+        non_none_default_t& operator =(non_none_default_t&& other) noexcept = delete;
+
+    public:
+        positive::integer_t Value() const
+        {
+            return this->value;
+        }
+    };
+
+    TEST_CASE("should work as a tag for types that don't use none as a default value")
+    {
+        non_none_default_t non_none_default = none::value_t<non_none_default_t>();
+
+        //CHECK(non_none_default.Value() == non_none_default_t::NONE);
+    }
+
 }
