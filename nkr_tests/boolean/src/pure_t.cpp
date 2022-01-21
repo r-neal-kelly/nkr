20014520ff8c26d3c9a7a4237762ee59d0aa544b
r-neal-kelly <r.neal.kelly@gmail.com>
2022-01-21 00:17:53 -0500

    started adding a nice template helper to test types with the tr trait syntax in bulk. this also avoids generating a function or anything like that in the binary, like our test suite template helper does.

--- nkr_tests/boolean/src/pure_t.cpp
+++ nkr_tests/boolean/src/pure_t.cpp
@@ -15,18 +15,62 @@
 
 namespace nkr {
 
+    // potential problem.
     static_assert(nkr::generic::boolean_tr<nkr::positive::integer_t>);
 
     TEST_SUITE("nkr::boolean::pure_t")
     {
+    #define nkr_TYPES(TYPE_QUALIFIER_p)         \
+        TYPE_QUALIFIER_p nkr::boolean::pure_t   \
+
+    #define nkr_JUST_NON_QUALIFIED  \
+        nkr_TYPES(nkr_BLANK)        \
+
+    #define nkr_JUST_CONST  \
+        nkr_TYPES(const)    \
+
+    #define nkr_JUST_VOLATILE   \
+        nkr_TYPES(volatile)     \
+
+    #define nkr_JUST_CONST_VOLATILE \
+        nkr_TYPES(const volatile)   \
+
+    #define nkr_ANY_QUALIFIED   \
+        nkr_JUST_CONST,         \
+        nkr_JUST_VOLATILE,      \
+        nkr_JUST_CONST_VOLATILE \
+
+    #define nkr_ANY_NON_QUALIFIED   \
+        nkr_JUST_NON_QUALIFIED      \
+
+    #define nkr_ANY_CONST       \
+        nkr_JUST_CONST,         \
+        nkr_JUST_CONST_VOLATILE \
+
+    #define nkr_ANY_NON_CONST   \
+        nkr_JUST_NON_QUALIFIED, \
+        nkr_JUST_VOLATILE       \
+
+    #define nkr_ANY_VOLATILE    \
+        nkr_JUST_VOLATILE,      \
+        nkr_JUST_CONST_VOLATILE \
+
+    #define nkr_ANY_NON_VOLATILE    \
+        nkr_JUST_NON_QUALIFIED,     \
+        nkr_JUST_CONST              \
+
+    #define nkr_ANY             \
+        nkr_JUST_NON_QUALIFIED, \
+        nkr_JUST_CONST,         \
+        nkr_JUST_VOLATILE,      \
+        nkr_JUST_CONST_VOLATILE \
+
         TEST_SUITE("identity")
         {
-            static_assert(nkr::boolean::pure_tr<nkr::boolean::pure_t>);
-            static_assert(nkr::boolean::pure_tr<const nkr::boolean::pure_t>);
-            static_assert(nkr::boolean::pure_tr<volatile nkr::boolean::pure_t>);
-            static_assert(nkr::boolean::pure_tr<const volatile nkr::boolean::pure_t>);
-
-            static_assert(sizeof(nkr::boolean::pure_t) == sizeof(nkr::boolean::cpp_t));
+            TEST_CASE_TEMPLATE("should satisfy the identity trait", pure_p, nkr_ANY)
+            {
+                static_assert(nkr::boolean::pure_tr<pure_p>);
+            }
         }
 
         TEST_SUITE("generic")
@@ -77,7 +121,218 @@ namespace nkr {
 
         TEST_SUITE("tr")
         {
+            TEST_SUITE("tr1")
+            {
+                template <nkr::boolean::cpp_t _, typename subject_p, typename tag_p, typename ...objects_p>
+                class tr1_tag_objects_tmpl;
 
+                template <typename subject_p, typename tag_p, typename ...objects_p>
+                class tr1_tag_objects_tmpl<false, subject_p, tag_p, objects_p...>
+                {
+                public:
+                    static constexpr nkr::boolean::cpp_t
+                        Is_True()
+                        noexcept
+                    {
+                        return false;
+                    }
+                };
+
+                template <typename subject_p, typename tag_p, typename last_object_p>
+                class tr1_tag_objects_tmpl<true, subject_p, tag_p, last_object_p>
+                {
+                public:
+                    static constexpr nkr::boolean::cpp_t
+                        Is_True()
+                        noexcept
+                    {
+                        return tr1<subject_p, tag_p, last_object_p>;
+                    }
+                };
+
+                template <typename subject_p, typename tag_p, typename first_object_p, typename ...more_objects_p>
+                class tr1_tag_objects_tmpl<true, subject_p, tag_p, first_object_p, more_objects_p...>
+                {
+                public:
+                    static constexpr nkr::boolean::cpp_t
+                        Is_True()
+                        noexcept
+                    {
+                        return tr1_tag_objects_tmpl<
+                            tr1<subject_p, tag_p, first_object_p>,
+                            subject_p,
+                            tag_p,
+                            more_objects_p...
+                        >::Is_True();
+                    }
+                };
+
+                template <nkr::boolean::cpp_t _, typename ...subjects_p>
+                class tr1_subjects_tmpl;
+
+                template <typename ...subjects_p>
+                class tr1_subjects_tmpl<false, subjects_p...>
+                {
+                public:
+                    template <typename tag_p, typename ...objects_p>
+                    static constexpr nkr::boolean::cpp_t
+                        Satisfy()
+                        noexcept
+                    {
+                        return false;
+                    }
+                };
+
+                template <typename last_subject_p>
+                class tr1_subjects_tmpl<true, last_subject_p>
+                {
+                public:
+                    template <typename tag_p, typename ...objects_p>
+                    static constexpr nkr::boolean::cpp_t
+                        Satisfy()
+                        noexcept
+                    {
+                        return tr1_tag_objects_tmpl<true, last_subject_p, tag_p, objects_p...>::Is_True();
+                    }
+                };
+
+                template <typename first_subject_p, typename ...more_subjects_p>
+                class tr1_subjects_tmpl<true, first_subject_p, more_subjects_p...>
+                {
+                public:
+                    template <typename tag_p, typename ...objects_p>
+                    static constexpr nkr::boolean::cpp_t
+                        Satisfy()
+                        noexcept
+                    {
+                        return tr1_subjects_tmpl<
+                            tr1_tag_objects_tmpl<true, first_subject_p, tag_p, objects_p...>::Is_True(),
+                            more_subjects_p...
+                        >::template Satisfy<tag_p, objects_p...>();
+                    }
+                };
+
+                template <typename ...subjects_p>
+                class tr1_subjects_t
+                {
+                public:
+                    template <typename tag_p, typename ...objects_p>
+                    static constexpr nkr::boolean::cpp_t
+                        Satisfy()
+                        noexcept
+                    {
+                        return tr1_subjects_tmpl<true, subjects_p...>::template Satisfy<tag_p, objects_p...>();
+                    }
+                };
+
+                static_assert(tr1_subjects_t<nkr_ANY>::Satisfy<any_tg, nkr::boolean::pure_t, nkr::boolean::pure_tg>());
+                static_assert(!tr1_subjects_t<nkr_ANY>::Satisfy<any_tg, nkr::boolean::cpp_t, nkr::boolean::cpp_tg>());
+            }
+
+            TEST_SUITE("tr1 any_tg")
+            {
+                TEST_CASE_TEMPLATE("should be true for any compared to nkr::boolean::pure_t", pure_p, nkr_ANY)
+                {
+                    static_assert(tr1<pure_p, any_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be true for any compared to nkr::boolean::pure_tg", pure_p, nkr_ANY)
+                {
+                    static_assert(tr1<pure_p, any_tg, nkr::boolean::pure_tg>);
+                }
+            }
+
+            TEST_SUITE("tr1 any_qualified_tg")
+            {
+                TEST_CASE_TEMPLATE("should be true for any qualified compared to nkr::boolean::pure_t", pure_p, nkr_ANY_QUALIFIED)
+                {
+                    static_assert(tr1<pure_p, any_qualified_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be true for any qualified compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_QUALIFIED)
+                {
+                    static_assert(tr1<pure_p, any_qualified_tg, nkr::boolean::pure_tg>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any non-qualified compared to nkr::boolean::pure_t", pure_p, nkr_ANY_NON_QUALIFIED)
+                {
+                    static_assert(!tr1<pure_p, any_qualified_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any non-qualified compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_NON_QUALIFIED)
+                {
+                    static_assert(!tr1<pure_p, any_qualified_tg, nkr::boolean::pure_tg>);
+                }
+            }
+
+            TEST_SUITE("tr1 any_non_qualified_tg")
+            {
+                TEST_CASE_TEMPLATE("should be true for any non-qualified compared to nkr::boolean::pure_t", pure_p, nkr_ANY_NON_QUALIFIED)
+                {
+                    static_assert(tr1<pure_p, any_non_qualified_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be true for any non-qualified compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_NON_QUALIFIED)
+                {
+                    static_assert(tr1<pure_p, any_non_qualified_tg, nkr::boolean::pure_tg>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any qualified compared to nkr::boolean::pure_t", pure_p, nkr_ANY_QUALIFIED)
+                {
+                    static_assert(!tr1<pure_p, any_non_qualified_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any qualified compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_QUALIFIED)
+                {
+                    static_assert(!tr1<pure_p, any_non_qualified_tg, nkr::boolean::pure_tg>);
+                }
+            }
+
+            TEST_SUITE("tr1 any_const_tg")
+            {
+                TEST_CASE_TEMPLATE("should be true for any const compared to nkr::boolean::pure_t", pure_p, nkr_ANY_CONST)
+                {
+                    static_assert(tr1<pure_p, any_const_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be true for any const compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_CONST)
+                {
+                    static_assert(tr1<pure_p, any_const_tg, nkr::boolean::pure_tg>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any non-const compared to nkr::boolean::pure_t", pure_p, nkr_ANY_NON_CONST)
+                {
+                    static_assert(!tr1<pure_p, any_const_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any non-const compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_NON_CONST)
+                {
+                    static_assert(!tr1<pure_p, any_const_tg, nkr::boolean::pure_tg>);
+                }
+            }
+
+            TEST_SUITE("tr1 any_non_const_tg")
+            {
+                TEST_CASE_TEMPLATE("should be true for any non-const compared to nkr::boolean::pure_t", pure_p, nkr_ANY_NON_CONST)
+                {
+                    static_assert(tr1<pure_p, any_non_const_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be true for any non-const compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_NON_CONST)
+                {
+                    static_assert(tr1<pure_p, any_non_const_tg, nkr::boolean::pure_tg>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any const compared to nkr::boolean::pure_t", pure_p, nkr_ANY_CONST)
+                {
+                    static_assert(!tr1<pure_p, any_non_const_tg, nkr::boolean::pure_t>);
+                }
+
+                TEST_CASE_TEMPLATE("should be false for any const compared to nkr::boolean::pure_tg", pure_p, nkr_ANY_CONST)
+                {
+                    static_assert(!tr1<pure_p, any_non_const_tg, nkr::boolean::pure_tg>);
+                }
+            }
         }
 
         TEST_SUITE("operators")
