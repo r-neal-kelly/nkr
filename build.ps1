c73b5b3bd34e91e2a757c94b3ef56d883c7a908e
r-neal-kelly <r.neal.kelly@gmail.com>
2022-03-29 03:16:58 -0400

    enhanced the build script by adding better parameters and a help message.

--- build.ps1
+++ build.ps1
@@ -1,24 +1,51 @@
 param(
+    [switch]$help,
     [string]$arch = "x64",
     [string]$mode = "Debug",
-    [boolean]$gen_tests = $false
+    [switch]$test
 )
 
-if ($gen_tests) {
-    $Env:NKR_GENERATE_TESTS = $true
+if ($help.IsPresent) {
+    Write-Host
+    Write-Host "Info:"
+    Write-Host "    Uses CMake to generate build files and then subsequently compiles the library."
+    Write-Host "    Requires CMake and Node.js to be installed and available in the PATH."
+    Write-Host "    Places all configurations in the './build' directory."
+    Write-Host
+    Write-Host "Parameters:"
+    Write-Host "    -help: Brings up this help message."
+    Write-Host "    -arch: The target architecture, e.g. 'x64', 'Win32'."
+    Write-Host "    -mode: The target mode, e.g. 'Debug', 'Release'."
+    Write-Host "    -test: Will build and compile the test suites."
+    Write-Host
+} else {
+    if ((Get-Command "node" -ErrorAction SilentlyContinue) -eq $null) {
+        Write-Host
+        Write-Host "This program requires Node.js to be installed and available in the PATH."
+        Write-Host "If you prefer, you can simply use CMake to manually generate and build the library."
+        Write-Host
+    } elseif ((Get-Command "cmake" -ErrorAction SilentlyContinue) -eq $null) {
+        Write-Host
+        Write-Host "This program requires CMake to be installed and available in the PATH."
+        Write-Host
+    } else {
+        if ($test.IsPresent) {
+            $Env:NKR_GENERATE_TESTS = $true
+        }
+
+        node "./tools/update_cmake_lists/update_cmake_lists" "./"
+
+        $path = Join-Path (Resolve-Path .) "build/$arch/$mode"
+
+        cmake -B "$path" -A "$arch"
+        cmake --build "$path" --config "$mode" --parallel
+
+        Write-Host
+        Write-Host "Generated and built project at $path"
+
+        Write-Host
+        Write-Host press any key to continue...
+        Write-Host
+        Read-Host
+    }
 }
-
-node "./tools/update_cmake_lists/update_cmake_lists" "./"
-
-$path = Join-Path (Resolve-Path .) "build/$arch/$mode"
-
-cmake -B "$path" -A "$arch"
-cmake --build "$path" --config "$mode" --parallel
-
-Write-Host
-Write-Host "Generated and built project at $path"
-
-Write-Host
-Write-Host press any key to continue...
-Write-Host
-Read-Host
